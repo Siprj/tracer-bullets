7b148ef8044a2c1141154d253c9208ba5f4f300a
Jan Sipr <sipr.jan@gmail.com>
2018-07-01 00:31:44 +0200

    My first triangle

--- opengl-playground/src/Lib.hs
+++ opengl-playground/src/Lib.hs
@@ -1,14 +1,22 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lib
     ( someFunc
     ) where
 
+import Control.Monad (unless, when, void)
 import Data.Maybe (fromJust)
 import Data.StateVar
-import Control.Monad (unless, when, void)
+import Data.ByteString (ByteString)
+import qualified Data.Vector.Storable as V
+import Foreign.Storable (sizeOf)
+import Foreign.Ptr (Ptr, plusPtr, nullPtr)
 import System.Exit (exitFailure)
+import System.IO (IO)
 -- GLFW-b, qualified for clarity
 import qualified Graphics.UI.GLFW as GLFW
 import qualified Graphics.Rendering.OpenGL as GL
+import qualified Graphics.Rendering.OpenGL.GL.BufferObjects as GL
 -- gl, all types and funcs here will already start with "gl"
 
 someFunc :: IO ()
@@ -37,17 +45,86 @@ someFunc = do
         putStrLn "alsdfjlaksjdflkajsdf "
         GLFW.setKeyCallback window (Just callback)
         GLFW.makeContextCurrent (Just window)
+
+        triangle <- GL.genObjectName
+        GL.bindBuffer GL.ArrayBuffer $= Just triangle
+        V.unsafeWith vertices $ \ptr -> do
+            let size = fromIntegral
+                    (V.length vertices * sizeOf (V.head vertices))
+            GL.bufferData GL.ArrayBuffer $= (size, ptr, GL.StaticDraw)
+
+        vertexShader <- GL.createShader GL.VertexShader
+        GL.shaderSourceBS vertexShader $= vertexShaderText
+        GL.compileShader vertexShader
+        verRes <- GL.compileStatus vertexShader
+        unless verRes $ putStrLn "vertec shader compilation FAILED"
+            >> exitFailure
+
+        fragmentShader <- GL.createShader GL.FragmentShader
+        GL.shaderSourceBS fragmentShader $= fragmentShaderText
+        GL.compileShader fragmentShader
+        fragRes <- GL.compileStatus fragmentShader
+        unless verRes $ putStrLn "fragmen shader compilation FAILED"
+            >> exitFailure
+
+        shaderProgram <- GL.createProgram
+        GL.attachShader shaderProgram vertexShader
+        GL.attachShader shaderProgram fragmentShader
+        GL.linkProgram shaderProgram
+        GL.linkStatus shaderProgram
+        GL.currentProgram $= Just shaderProgram
+
+        GL.deleteObjectName vertexShader
+        GL.deleteObjectName fragmentShader
+
+        vao <- GL.genObjectName
+        GL.bindVertexArrayObject $= Just vao
+        GL.vertexAttribPointer (GL.AttribLocation 0)
+            $= ( GL.ToFloat
+               , GL.VertexArrayDescriptor 3 GL.Float 0 (bufferOffset 0)
+               )
+        GL.vertexAttribArray (GL.AttribLocation 0) $= GL.Enabled
+
         loop window
 
     loop window = do
         shouldClose <- GLFW.windowShouldClose window
         unless shouldClose $ do
             GLFW.pollEvents
-            GL.clearColor $= (GL.Color4 0.2 0.3 0.3 1.0)
+            GL.clearColor $= GL.Color4 0.2 0.3 0.3 1.0
             GL.clear [GL.ColorBuffer]
+            GL.drawArrays GL.Triangles 0 3
             GLFW.swapBuffers window
             loop window
 
+    vertices :: V.Vector (GL.Vertex3 GL.GLfloat)
+    vertices = V.fromList
+        [ GL.Vertex3 (-1.0) (-1.0) 0.0
+        , GL.Vertex3 1.0 (-1.0) 0.0
+        , GL.Vertex3 0.0  1.0 0.0
+        ]
+
+
+    bufferOffset :: Integral a => a -> Ptr b
+    bufferOffset = plusPtr nullPtr . fromIntegral
+
+    vertexShaderText :: ByteString
+    vertexShaderText =
+        "#version 330 core\n\
+        \layout (location = 0) in vec3 aPos;\n\
+        \void main()\n\
+        \{\n\
+        \    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n\
+        \}\n"
+
+    fragmentShaderText :: ByteString
+    fragmentShaderText =
+        "#version 330 core\n\
+        \out vec4 FragColor;\n\
+        \void main()\n\
+        \{\n\
+        \    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n\
+        \}\n"
 
 callback :: GLFW.KeyCallback
 callback window key scanCode keyState modKeys = do
